a236372d4d93a8e5596bfc59e213036a8740c7bf
Eli <buchereli@gmail.com>
2017-06-08 19:50:19 -0400

    Initial commit

---
+++ desktop/src/com/ebucher/stackables/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.ebucher.stackables.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.ebucher.stackables.StackablesMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new StackablesMain(), config);
+	}
+}
